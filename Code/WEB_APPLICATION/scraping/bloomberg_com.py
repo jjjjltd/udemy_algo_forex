ae7d097ebd272887a0e193c6992c0551c6338f73
jjjjltd <johnjoyce90@hotmail.co.uk>
2023-09-28 15:39:53 +0100

    Web App v0.2.

---
+++ Code/WEB_APPLICATION/scraping/bloomberg_com.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import cloudscraper
+
+def get_article(card):
+    return dict(
+        headline=card.get_text(),
+        link='https://www.reuters.com' + card.get('href')
+    )
+
+
+def bloomberg_com():
+
+    s = cloudscraper.create_scraper()
+
+    headers = {
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"
+    }
+
+    resp = s.get("https://www.reuters.com/business/finance/", headers=headers)
+
+    soup = BeautifulSoup(resp.content, 'html.parser')
+
+    links = []
+    cards = soup.select('[class^="media-story-card__body"]')
+    for card in cards:
+        ca = card.find('a', {'data-testid': 'Heading'})
+        links.append(get_article(ca))
+    return links
+
+
+    
